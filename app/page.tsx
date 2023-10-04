5f3dcf51a6c081ee8321844cec3d8b366ab7e480
Manjil Tamang <lamamanjil@gmail.com>
2023-10-04 23:26:21 +0545

    minor updates, WIP: vercel deployment

--- app/page.tsx
+++ app/page.tsx
@@ -5,5 +5,9 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const session = await getServerSession(options);
   if (session) redirect("/dashboard");
-  return <main>👋 Hello there</main>;
+  return (
+    <main className="px-[15px] py-4">
+      <h1 className="text-5xl font-bold">👋 Hello there</h1>
+    </main>
+  );
 }
