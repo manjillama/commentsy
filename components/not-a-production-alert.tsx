a7e52bebec70a64b3b2efeb921bc0a5a18d3f576
Manjil Tamang <lamamanjil@gmail.com>
2023-10-31 22:25:41 +0545

    update warn message

--- components/not-a-production-alert.tsx
+++ components/not-a-production-alert.tsx
@@ -3,18 +3,25 @@
 export default function NotAProductionAlert() {
   return (
     <div className="p-3 text-black bg-amber-300">
-      Please note that this is not a real project. Hence it is recommended to
-      rather{" "}
+      Please note that this is not a real project. It is highly recommended to{" "}
+      <a
+        href="https://github.com/manjillama/commentsy/fork"
+        target="_blank"
+        rel="noreferrer"
+        className="underline"
+      >
+        fork
+      </a>{" "}
+      this project and{" "}
       <a
         href="https://vercel.com/new"
         target="_blank"
         rel="noreferrer"
         className="underline"
       >
-        self host
+        self-host
       </a>{" "}
-      this project on your own and then use it to prevent any potential data
-      losses.
+      it to prevent any potential data losses.
     </div>
   );
 }
